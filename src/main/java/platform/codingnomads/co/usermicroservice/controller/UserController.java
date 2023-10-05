1cdb41bf03a88cceca354a71fd099d235f64bf0a
sam <sam.leefr@gmail.com>
2023-10-05 14:39:24 +0200

    db

--- src/main/java/platform/codingnomads/co/usermicroservice/controller/UserController.java
+++ src/main/java/platform/codingnomads/co/usermicroservice/controller/UserController.java
@@ -1,7 +1,6 @@
 package platform.codingnomads.co.usermicroservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import platform.codingnomads.co.usermicroservice.model.User;
